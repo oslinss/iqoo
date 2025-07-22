05e35bc9265f83548c70f26862b4b231083b3126
linxxxxhhhh <15800368+linxxxxhhhh@user.noreply.gitee.com>
2025-07-22 18:56:01 +0800

    update

--- components/Survey.vue
+++ components/Survey.vue
@@ -10,7 +10,7 @@
                 <p>iQOO官方特邀宁填答这份问卷，将您真实的感受和建议告诉我们。您所填的全部信息我们将严格保密，只用于内部分析与改进体验使用，不用与任何商业目的。如已知悉，请您开始答题；反之，您可退出此页面。</p>
             </div>
             <div class="button">
-                <el-button type="primary">开始答题</el-button>
+                <el-button type="primary"><a class="btn" href="https://ai-h5.vivo.com.cn/special-questionnairev2/index.html?id=3125&sign=c99009cd5dd1ff78caf9fae7a101e651#/singlequestion">开始答题</a></el-button>
             </div>
         </div>
     </div>
@@ -38,18 +38,6 @@
     text-align: center;
 }
 
-.button {
-    margin-top: 200px;
-}
-
-.button .el-button {
-    color: white !important;
-    text-decoration: none;
-    background-color: #415fff !important;
-    border: #415fff;
-    width: 150px;
-}
-
 h1 {
     font-size: 20px;
     color: #303133;
@@ -59,4 +47,21 @@ p {
     font-size: 15px;
     color:#606266;
 }
+
+.button {
+    margin-top: 200px;
+}
+
+.button .el-button {
+    
+    background-color: #415fff !important;
+    border: #415fff;
+    width: 150px;
+}
+
+.btn {
+    color: white !important;
+    text-decoration: none;
+}
+
 </style>
